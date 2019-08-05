1cc3669d1e5a5ddc663d17ffe6192c4702003b81
big.rice.eater <34292181+BigRiceEater@users.noreply.github.com>
2019-08-05 11:19:29 +0800

    Warn if credentials are wrong

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,21 @@
 const router = require('express').Router();
 
 router.get('/login', (req, res) => {
-  res.render('login');
+  const { warn } = req.query;
+  console.log(warn);
+  res.render('login', { wrongCredentials: warn });
 });
 
 router.post('/login', (req, res) => {
   const { username, password } = req.body;
   console.log(username, password);
-  req.session.name = username;
-  res.redirect('/');
+
+  if (password === 'opensesame') {
+    req.session.name = username;
+    res.redirect('/');
+  } else {
+    res.redirect('/login?warn=true');
+  }
 });
 
 router.get('/logout', (req, res) => {
